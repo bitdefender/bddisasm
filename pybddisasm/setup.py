10dc00681d684ca7fc11746a55fea89c82ff003b
Andrei Vlad LUTAS <vlutas@bitdefender.com>
2021-05-17 09:13:27 +0300

    Updated version for pybddisasm build.

--- pybddisasm/setup.py
+++ pybddisasm/setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages, setup, Command, Extension, Distribution
 from codecs import open
 
 VERSION = (0, 1, 3)
-LIBRARY_VERSION = (1, 32, 1)
+LIBRARY_VERSION = (1, 32, 3)
 LIBRARY_INSTRUX_SIZE = 864
 
 packages = ['pybddisasm']
