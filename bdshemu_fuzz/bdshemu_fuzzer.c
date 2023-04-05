d16f1d8ba346a301ed32d28b6384472f18c1992e
Ionel-Cristinel ANICHITEI <ianichitei@bitdefender.com>
2023-04-05 11:06:10 +0300

    bdshemu_fuzz: Update build scripts

--- bdshemu_fuzz/bdshemu_fuzzer.c
+++ bdshemu_fuzz/bdshemu_fuzzer.c
@@ -2,16 +2,22 @@
  * Copyright (c) 2020 Bitdefender
  * SPDX-License-Identifier: Apache-2.0
  */
+#ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 
+#include <limits.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
 #include "bddisasm.h"
 #include "bdshemu.h"
+
+#pragma clang optimize off
+#pragma GCC            optimize("O0")
 
 #ifdef ENABLE_LOGGING
 #define LOG(fmt, ...)   printf(fmt, ##__VA_ARGS__)
@@ -19,65 +25,20 @@
 #define LOG(fmt, ...)
 #endif // ENABLE_LOGGING
 
-#ifndef __AFL_LOOP
-#warning "Persistent mode will not work (you are probably not using afl-clang-fast)"
-#endif
-
-int nd_vsnprintf_s(
-    char *buffer,
-    size_t sizeOfBuffer,
-    size_t count,
-    const char *format,
-    va_list argptr
-)
-{
-    return vsnprintf(buffer, sizeOfBuffer, format, argptr);
-}
-
-void *nd_memset(void *s, int c, size_t n)
-{
-    return memset(s, c, n);
-}
-
-uint8_t *read_file(const char *fpath, size_t *size)
-{
-    uint8_t *buffer;
-    FILE *fd = fopen(fpath, "rb");
-    if (fd == NULL)
-    {
-        LOG("[-] Failed to open \"%s\"\n", fpath);
-        return NULL;
-    }
-
-    fseek(fd, 0ull, SEEK_END);
-    *size = ftell(fd);
-    rewind(fd);
-
-    buffer = malloc(*size);
-    if (buffer == NULL)
-    {
-        LOG("[-] Failed to allocate %zu bytes\n", *size);
-    }
-    else
-    {
-        fread(buffer, *size, 1, fd);
-    }
-
-    fclose(fd);
-    return buffer;
-}
-
-static bool gUseLog;
-
 void ShemuLog(char *data)
 {
-    if (gUseLog)
-    {
-        LOG("%s", data);
-    }
+    LOG("%s", data);
 }
 
-void run_shemu(uint8_t *Data, size_t Size, uint8_t Def)
+#ifdef FUZZ_X86
+#define DEF_CODE    ND_CODE_32
+#define FUZZER_TYPE "x86"
+#else
+#define DEF_CODE    ND_CODE_64
+#define FUZZER_TYPE "x64"
+#endif // FUZZ_X86
+
+void run_shemu(uint8_t *Data, size_t Size)
 {
     // More or less copy pasted from disasmtool
 
@@ -119,7 +80,7 @@ void run_shemu(uint8_t *Data, size_t Size, uint8_t Def)
     ctx.Segments.Gs.Selector = 0x30;
     ctx.Segments.Gs.Base = 0x7FFF0000;
 
-    ctx.Mode = Def;
+    ctx.Mode = DEF_CODE;
     ctx.Ring = 3;
     ctx.TibBase = ctx.Mode == ND_CODE_32 ? ctx.Segments.Fs.Base : ctx.Segments.Gs.Base;
     ctx.MaxInstructionsCount = 4096;
@@ -134,85 +95,87 @@ void run_shemu(uint8_t *Data, size_t Size, uint8_t Def)
     free(ctx.Stack);
 }
 
-void run_test(uint8_t *Data, size_t Size, uint8_t Def)
-{
-    run_shemu(Data, Size, Def);    
-}
+#if defined(__AFL_FUZZ_TESTCASE_LEN)
+#include <unistd.h>
 
-int8_t arch_to_def_code(const char *arch)
-{
-    uint32_t a = strtoul(arch, NULL, 0);
+// See https://github.com/AFLplusplus/AFLplusplus/blob/stable/instrumentation/README.persistent_mode.md
+__AFL_FUZZ_INIT();
 
-    switch (a)
+int main()
+{
+    __AFL_INIT();
+
+    uint8_t *buffer = __AFL_FUZZ_TESTCASE_BUF;
+    while (__AFL_LOOP(UINT_MAX))
     {
-    case 16:
-        return ND_CODE_16;
-
-    case 32:
-        return ND_CODE_32;
-
-    case 64:
-        return ND_CODE_64;
-
-    default:
-        LOG("[-] Unknown arch: \"%s\"\n", arch);
-        return -1;
+        size_t size = __AFL_FUZZ_TESTCASE_LEN;
+        run_shemu(buffer, size);
     }
+
+    return 0;
+}
+#elif defined(__clang__)
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+    // Shemu will write into the shellcode buffer, so copy it to a new buffer first.
+
+    uint8_t *buffer = malloc(Size);
+    if (!buffer) return 1;
+
+    memcpy(buffer, Data, Size);
+    run_shemu(buffer, Size);
+
+    free(buffer);
+    return 0;
+}
+#else
+uint8_t *read_file(const char *fpath, size_t *size)
+{
+    uint8_t *buffer;
+    FILE *fd = fopen(fpath, "rb");
+    if (fd == NULL)
+    {
+        LOG("[-] Failed to open \"%s\"\n", fpath);
+        return NULL;
+    }
+
+    fseek(fd, 0ull, SEEK_END);
+    *size = ftell(fd);
+    rewind(fd);
+
+    buffer = malloc(*size);
+    if (buffer == NULL)
+    {
+        LOG("[-] Failed to allocate %zu bytes\n", *size);
+    }
+    else
+    {
+        fread(buffer, *size, 1, fd);
+    }
+
+    fclose(fd);
+    return buffer;
 }
 
 int main(int argc, char **argv)
 {
-    size_t size = 0;
-    uint8_t *buffer;
-    int8_t defCode;
-
-    if (argc < 3)
+    if (argc < 2)
     {
-        LOG("[-] Missing arguments: <file path> <16|32|64> [activate shemu print]\n");
+        LOG("Missing file argument\n");
         abort();
     }
 
-    defCode = arch_to_def_code(argv[2]);
-    if (defCode == -1)
+    size_t size;
+    uint8_t *buffer = read_file(argv[1], &size);
+    if (!buffer)
     {
+        LOG("[-] read_file failed for \"%s\"\n", argv[1]);
         abort();
     }
 
-    if (argc >= 4)
-    {
-        LOG("[+] Will activate ShemuLog\n");
-        gUseLog = true;
-    }
-    else
-    {
-        gUseLog = false;
-    }
-
-    LOG("[+] Using def code/data = %d\n", defCode);
-
-// __AFL_LOOP is defined only when compiling with afl-clang-fast and allows us to use
-// AFL's persistent mode. We still want to be able to compile and run even if we are
-// using afl-gcc or afl-clang, but there is no reason to actually have a loop here
-// if we are not using persistent mode.
-#ifdef __AFL_LOOP
-    while (__AFL_LOOP(1000))
-#endif // __AFL_LOOP
-    {
-        LOG("[+] Reading data from \"%s\"\n", argv[1]);
-        buffer = read_file(argv[1], &size);
-        if (buffer == NULL)
-        {
-            abort();
-        }
-
-        LOG("[+] Read %zu bytes at %p (range: [%p, %p))\n", size, buffer, buffer, buffer + size);
-
-        run_test(buffer, size, (uint8_t)defCode);
-
-        free(buffer);
-    }
-
-    LOG("[+] Done!\n");
+    run_shemu(buffer, size);
+    free(buffer);
 
     return 0;
 }
+#endif // defined(__AFL_FUZZ_TESTCASE_LEN)
